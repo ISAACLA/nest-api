35efd76cb55a59ef101f71844bb3dba73bef9c7e
ISAACLA <yanbinla@gmail.com>
2020-03-11 23:45:26 -0700

    Creating new task using DTO

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
 import { TasksService } from "./tasks.service";
 import { Task } from "./task.model";
+import { TaskDto } from "./dto/create-task.dto";
 
 @Controller('tasks')
 export class TasksController {
@@ -12,11 +13,22 @@ export class TasksController {
         return this.tasksService.getTasks();
     }
 
+    /*
+    * Post without using DTO
+    * */
+    // @Post()
+    //     // createTask(
+    //     //     @Body('title') title: string,
+    //     //     @Body('content') content: string
+    //     // ): Task {
+    //     //     return this.tasksService.createTask(title, content);
+    //     // }
+
+    /*
+    * Post using DTO
+    * */
     @Post()
-    createTask(
-        @Body('title') title: string,
-        @Body('content') content: string
-    ): Task {
-        return this.tasksService.createTask(title, content);
+    createTask(taskDto: TaskDto) {
+        return this.tasksService.createTask(taskDto)
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,23 +1,49 @@
-import {Injectable} from '@nestjs/common';
-import {v4 as uuidv4} from "uuid";
-import {tasksMock} from "./task.mock";
-import {Task, TaskStatus} from "./task.model";
+import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from "uuid";
+import { tasksMock } from "./task.mock";
+import { Task, TaskStatus } from "./task.model";
+import { TaskDto } from "./dto/create-task.dto";
 
 @Injectable()
 export class TasksService {
 
+    tasks:Task[] = tasksMock;
+
     getTasks(): Task[]{
-        return tasksMock;
+        return this.tasks;
     }
 
-    createTask(title: string, content: string): Task {
-        // tasksMock.push(task);
+    /*
+    * Service create function without using DTO
+    * */
+    // createTask(title: string, content: string): Task {
+    //     const task:Task = {
+    //         id: uuidv4(),
+    //         title,
+    //         content,
+    //         status: TaskStatus.OPEN
+    //     };
+    //
+    //     this.tasks.push(task);
+    //
+    //     return task;
+    // }
 
-        return {
+    /*
+    * Service createTask use DTO
+    * */
+
+    createTask(taskDto: TaskDto): Task {
+        const { title, content } = taskDto;
+
+        const task: Task = {
             id: uuidv4(),
             title,
             content,
             status: TaskStatus.OPEN
         };
+
+        this.tasks.push(task);
+        return task;
     }
 }
